d4536878326d2a3092dcc7b30ed226b70fd9f7b2
Stefan Buhrmester <buhrmi@gmail.com>
2020-07-24 01:20:54 +0100

    fix testcase

--- test/runtime/samples/await-containing-outro/_config.js
+++ test/runtime/samples/await-containing-outro/_config.js
@@ -1,11 +1,9 @@
-
 export default {
-
-	html: ``,
-
-	async test({ assert, component, target }) {
-
-    await(new Promise(r => setTimeout(r, 1000)))
+	async test({ assert, component, target, window, raf }) {
+    for (let i = 0; i < 300; i++) {
+      raf.tick(i)
+      await(new Promise(r => setTimeout(r, 1)))
+    }
     
     assert.htmlEqual(target.innerHTML, `
       the value is <span>2</span>
--- test/runtime/samples/await-containing-outro/main.svelte
+++ test/runtime/samples/await-containing-outro/main.svelte
@@ -1,11 +1,11 @@
 <script>
 	import { fade } from 'svelte/transition'
   let value = 0
-	setTimeout(() => {value = 1}, 50)
-	setTimeout(() => {value = 2}, 100)
+	setTimeout(() => {value = 1}, 10)
+	setTimeout(() => {value = 2}, 200)
 </script>
 
 the value is
 {#await Promise.resolve(value) then x}
-  <span out:fade={{duration: 0}}>{x}</span>
+  <span out:fade={{duration: 50}}>{x}</span>
 {/await}
